dda645f52edfbc2d9b18d4bc0988062dbee841bc
carleen Thio <carleenthio@yahoo.com>
2017-09-26 22:12:47 -0400

    Finish basic project
    It multiplies cat age by 7.
    
    Problem:
    Assumes that there will always be an (int) input. Program crash if it's blank or not an int.
    
    Solution:
    make sure it's int by changing keyboard type to numpad.
    
    Unsolved:
    Nothing for the crash yet

--- Cat-Years/ViewController.swift
+++ Cat-Years/ViewController.swift
@@ -4,12 +4,23 @@
 //
 //  Created by Carleen Thio on 2017-09-26.
 //  Copyright © 2017 SCT. All rights reserved.
-//
+// Problem:
+// Assumes that there will always be an (int) input.
+// Program crash if it's blank or not an int.
+// Solution:
+// make sure it's int by changing keyboard type to numpad.
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var catAge: UILabel!
+    @IBOutlet weak var ageInput: UITextField!
+    
+    @IBAction func submitButton(_ sender: Any) {
+        let answer = Int(ageInput.text!)! * 7
+        catAge.text = String(answer)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
